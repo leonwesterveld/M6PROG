a080217b2b519a3e1991174f386f84c01f8bab64
Leon Westerveld <35258@ma-web.nl>
2023-12-11 15:26:07 +0100

    les 2 af

--- m6prog_project/public/nawOpslaan.php
+++ m6prog_project/public/nawOpslaan.php
@@ -1,2 +1,54 @@
 <?php
-    
+    echo '<pre>';
+    var_dump( $_POST );
+    echo '</pre>';
+
+    $naam = '';
+    echo '<b>naam: </b>';
+    if ( empty( $_POST['naam']) ) {
+        echo '<b style="color:#f00;">Je moet wel je naam invullen</b>';
+    } else {
+        $naam = $_POST['naam'];
+    }
+    echo $naam;
+    echo '<br>';
+
+    $straat = '';
+    echo '<b>straat: </b>';
+    if ( empty( $_POST['straat']) ) {
+        echo '<b style="color:#f00;">Je moet wel je straat invullen</b>';
+    } else {
+        $straat = $_POST['straat'];
+    }
+    echo $straat;
+    echo '<br>';
+
+    $huisnummer = '';
+    echo '<b>huisnummer: </b>';
+    if ( empty( $_POST['huisnummer']) ) {
+        echo '<b style="color:#f00;">Je moet wel je huisnummer invullen</b>';
+    } else {
+        $huisnummer = $_POST['huisnummer'];
+    }
+    echo $huisnummer;
+    echo '<br>';
+
+    $postcode = '';
+    echo '<b>postcode: </b>';
+    if ( empty( $_POST['postcode']) ) {
+        echo '<b style="color:#f00;">Je moet wel je postcode invullen</b>';
+    } else {
+        $postcode = $_POST['postcode'];
+    }
+    echo $postcode;
+    echo '<br>';
+
+    $email = '';
+    echo '<b>email: </b>';
+    if ( empty( $_POST['email']) ) {
+        echo '<b style="color:#f00;">Je moet wel je email invullen</b>';
+    } else {
+        $email = $_POST['email'];
+    }
+    echo $email;
+    echo '<br>';
--- m6prog_project/public/search.php
+++ m6prog_project/public/search.php
@@ -1,4 +1,15 @@
 <?php
-    echo '<pre>';
-    var_dump( $_GET['zoeken'] );
-    echo '</pre>';
+    require_once '../source/config.php';
+    require_once SOURCE_ROOT . '/database.php';
+    $connection = database_connect();
+    $plaats = $_GET["zoeken"];
+    $sql = 'SELECT * FROM weersomstandighedenPerDag WHERE Plaats=? ORDER BY Datum'; 
+    $stmt = $connection->prepare($sql);
+    $stmt->bind_param('s', $plaats);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    while( $weersomstandigheid = mysqli_fetch_assoc($result) ) {
+        var_dump( $weersomstandigheid );
+        echo "</br>";
+     }
