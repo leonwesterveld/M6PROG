5cc5a11583c3d601113cf4fca09a1a7148fd767c
Leon Westerveld <35258@ma-web.nl>
2024-01-09 02:43:15 +0100

    04 inserts tot 05 af

--- m6prog_project/public/assets/js/app.js
+++ m6prog_project/public/assets/js/app.js
@@ -4,17 +4,28 @@ function searchPersoon(event) {
     event.preventDefault();
     let form = event.target;
     const data = new FormData(form);
-    let url = "searchNaw.php?search=" + data.get("searchPersoon");
+    let url = "searchNaw.php?searchPersoon=" + data.get("searchPersoon");
     console.log(url);
 
     fetch(url)
         .then(async (response) => {
-            console.log(response);
             let json = await response.json();
-            console.log(json)
+            console.log(json);
+            showPersoon(json);
         });
 }
 
 searchFormulier.addEventListener('submit', (event) => {
     searchPersoon(event);
 });
+
+function showPersoon(json) {
+    let person = json[0];
+    document.getElementById("naam").textContent = person.naam;
+    document.getElementById("id").textContent = person.idnaw;
+    document.getElementById("straat").textContent = person.straat;
+    document.getElementById("huisnummer").textContent = person.huisnummer;
+    document.getElementById("postcode").textContent = person.postcode;
+    document.getElementById("email").textContent = person.email;
+    console.log(person);
+}
--- m6prog_project/public/nawOpslaan.php
+++ m6prog_project/public/nawOpslaan.php
@@ -1,54 +1,22 @@
 <?php
-    echo '<pre>';
-    var_dump( $_POST );
-    echo '</pre>';
+    
+include_once("../source/database.php");
+require_once '../source/config.php';
 
-    $naam = '';
-    echo '<b>naam: </b>';
-    if ( empty( $_POST['naam']) ) {
-        echo '<b style="color:#f00;">Je moet wel je naam invullen</b>';
-    } else {
-        $naam = $_POST['naam'];
-    }
-    echo $naam;
-    echo '<br>';
+$data = file_get_contents('php://input');
+$json = json_decode($data);
 
-    $straat = '';
-    echo '<b>straat: </b>';
-    if ( empty( $_POST['straat']) ) {
-        echo '<b style="color:#f00;">Je moet wel je straat invullen</b>';
-    } else {
-        $straat = $_POST['straat'];
-    }
-    echo $straat;
-    echo '<br>';
+$conn = database_connect();
+$q = "INSERT INTO naw (naam, email, straat, huisnummer, postcode) VALUES (?, ?, ?, ?, ?)";
+$stmt = $conn->prepare($q);
+$stmt->bind_param( "sssss",
+                    $json->naam,
+                    $json->email,
+                    $json->straat,
+                    $json->huisnummer,
+                    $json->postcode
+                );
+$stmt->execute();
+$result = $stmt->execute();
+$response=["succeeded" => $result];
 
-    $huisnummer = '';
-    echo '<b>huisnummer: </b>';
-    if ( empty( $_POST['huisnummer']) ) {
-        echo '<b style="color:#f00;">Je moet wel je huisnummer invullen</b>';
-    } else {
-        $huisnummer = $_POST['huisnummer'];
-    }
-    echo $huisnummer;
-    echo '<br>';
-
-    $postcode = '';
-    echo '<b>postcode: </b>';
-    if ( empty( $_POST['postcode']) ) {
-        echo '<b style="color:#f00;">Je moet wel je postcode invullen</b>';
-    } else {
-        $postcode = $_POST['postcode'];
-    }
-    echo $postcode;
-    echo '<br>';
-
-    $email = '';
-    echo '<b>email: </b>';
-    if ( empty( $_POST['email']) ) {
-        echo '<b style="color:#f00;">Je moet wel je email invullen</b>';
-    } else {
-        $email = $_POST['email'];
-    }
-    echo $email;
-    echo '<br>';
